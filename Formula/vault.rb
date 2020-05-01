39bca955ccecce184cc7c718ce1936cd15adae7f
farnejb <46055195+farnejb@users.noreply.github.com>
2020-05-01 12:24:16 -0400

    Update vault.rb

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -1,6 +1,6 @@
 class Vault < Formula
-  VAULT_VERSION = "1.3.1".freeze
-  VAULT_SHA256  = "23abadfcda33d5dffa238284c68061d6938aaaa11324301c042fe8d00b83fa7f".freeze
+  VAULT_VERSION = "1.4.1".freeze
+  VAULT_SHA256  = "4ec90cc96259fcabb090180f85a1d4f67af88d8e21a84cb0102df22cb01e360c".freeze
 
   desc "Installs Vault from pre-built binaries"
   homepage "https://vaultproject.io/downloads.html"
